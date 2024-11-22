30a86942c1eccf8afc2be9067461a4b7230dcb17
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2024-11-22 12:23:30 +0100

    first commit

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIntro2024.Controllers
+{
+    public class UsersController : Controller
+    {
+        private List<string> users = new List<string>
+        { "Paperino", "Pippo", "Topolino", "Minnie"};
+
+        public IActionResult Index()
+        {
+            string listToPrint = "Elenco Utenti:";
+            foreach (var item in users)
+            {
+                listToPrint += $"\n{item}";
+            }
+
+            return Content(listToPrint);
+        }
+
+        public IActionResult Detail(int id)
+        {
+            string message = $"Sono Detail e ho ricevuto id: {id}";
+            message += $"\nL'utente richiesto è {users[id]}";
+            return Content(message);
+        }
+    }
+}
