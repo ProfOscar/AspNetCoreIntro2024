1408daa300705b3482046221b9ecb4a067be74ca
Oscar Cambieri <oscar.cambieri@vallauri.edu>
2025-01-17 12:30:15 +0100

    Introduzione alle WebApi

---
+++ Api/TestApiController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreIntro2024.Controllers.Api
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TestApiController : ControllerBase
+    {
+        public IActionResult Get()
+        {
+            var pet = new { Age = 44, Name = "Mary" };
+            return Ok(pet);
+        }
+    }
+}
---
+++ Api/UsersApiController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AspNetCoreIntro2024.Services;
+
+namespace AspNetCoreIntro2024.Api
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UsersApiController : ControllerBase
+    {
+        private IUsersService _usersService;
+
+        public UsersApiController(IUsersService usersService)
+        {
+            _usersService = usersService;
+        }
+
+        // GET api/users
+        public IActionResult Get()
+        {
+            var users = _usersService.GetUsers();
+            return Ok(users);
+        }
+
+        // GET api/users/cerca/{id utente}
+        [HttpGet("cerca/{userId}")]
+        // GET api/users/{id utente}
+        [HttpGet("{userId}")]
+        public IActionResult GetUserDetail(int userId)
+        {
+            var user = _usersService.GetUserById(userId);
+            return Ok(user);
+        }
+    }
+}
